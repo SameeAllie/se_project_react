d8a4752d3d1a2d532b9fceb5ebbc242567eaf212
Json Todd <98905482+SameeAllie@users.noreply.github.com>
2023-07-05 20:23:49 +0200

    Add files via upload

---
+++ src/components/Profile.js
@@ -0,0 +1,37 @@
+import React from "react";
+import SideBar from "./SideBar";
+import ClothesSection from "./ClothesSection";
+
+const Profile = ({
+  items,
+  onCardClick,
+  onAddClick,
+  isLoggedIn,
+  editClick,
+  logoutClick,
+  onLike,
+  onUnlike,
+}) => {
+  return (
+    <section className="profile">
+      <div className="profile__content">
+        <SideBar
+          isLoggedIn={isLoggedIn}
+          editClick={editClick}
+          logoutClick={logoutClick}
+        />
+        <div className="profile__info">
+        </div>
+        <ClothesSection
+          cards={items}
+          onCardClick={onCardClick}
+          onAddClick={onAddClick}
+          isLoggedIn={isLoggedIn}
+          onLike={onLike}
+          onUnlike={onUnlike}
+        />
+      </div>
+    </section>
+  );
+};
+export default Profile;
