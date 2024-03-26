03fcb0dfc8aaed136a8732d853b9a5a2fea5da7e
Json Todd <98905482+SameeAllie@users.noreply.github.com>
2024-03-26 15:21:41 +0000

    Update auth.js

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,7 +3,7 @@ import { checkResponse } from "../utils/constants";
 export const baseUrl =
   process.env.NODE_ENV === "production"
     ? "https://api.wtwrallie.crabdance.com"
-    : "http://localhost:3001";
+    : "http://localhost:3000";
 
 export const signUp = (user) => {
   return fetch(`${baseUrl}/signup`, {
